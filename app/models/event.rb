a32e745860f2af281ad8e43869b27318992ef849
Salvador <salvador.on@outlook.com>
2020-03-20 13:18:47 -0300

    fix issues

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,9 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: 'User', :foreign_key => "creator_id"
+  belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :attendances
   validates :event_date, presence: true
   default_scope -> { order(event_date: :asc) }
 
-  scope :upcoming,  -> { where('event_date >= ?', Time.now.strftime("%Y-%d-%m")) }
-  scope :past,      -> { where('event_date < ?', Time.now.strftime("%Y-%d-%m")) }
-
-
+  scope :upcoming, -> { where('event_date >= ?', Time.now.strftime('%Y-%d-%m')) }
+  scope :past, -> { where('event_date < ?', Time.now.strftime('%Y-%d-%m')) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_many :events, :foreign_key => "creator_id"
+  has_many :events, foreign_key: 'creator_id'
   has_many :attendances
   before_validation :record_signup, on: :create
 
