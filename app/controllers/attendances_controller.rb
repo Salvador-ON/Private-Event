aed85f138fa6845024e816bbf00f02c7420cd3de
Salvador <salvador.on@outlook.com>
2020-03-22 14:56:25 -0300

    solve rubocop issues

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,7 @@
 class AttendancesController < ApplicationController
   def new; end
 
-   def create
+  def create
     f1 = Attendance.create(user_id: current_user.id, event_id: params[:id])
     f1.save
     redirect_to '/'
--- spec/controllers/attendances_controller_spec.rb
+++ spec/controllers/attendances_controller_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AttendancesController, type: :controller do
-
-  describe "GET #create" do
-    it "returns http success" do
+  describe 'GET #create' do
+    it 'returns http success' do
       get :create
       expect(response).to have_http_status(:success)
     end
   end
-
 end
